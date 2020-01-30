832937d8b62eedd9e1943f3c25de872d76180f2f
VMware Code <noreply@vmware.com>
2020-01-30 21:35:45 +0000

    Initial commit by wroever

--- snippet.py
+++ snippet.py
@@ -39,24 +39,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             status_code=405
     )
 
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
-
 def parse_signature(value):
     parts = value.split('&')
     ret = {}
